b29ea941bb3fe0167b9fe85e75dcd775c374e91d
AEWEB <sohara@gh11.genewg01.local>
2014-09-09 19:49:24 +0900

    メール実装

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,6 +3,7 @@ class Task < ActiveRecord::Base
   def self.getTasks(user)
     Task.where(user_id:user.id).order("tasks.priority asc").order("tasks.start_time asc")
   end
+=begin
   def self.sendMail(user)
     tasks=self.getTasks(user)
     mailtext=I18n.t("reportmail.header").gsub(/name/, I18n.t("reportmail.name")) +I18n.t("usual.newline");
@@ -18,4 +19,6 @@ class Task < ActiveRecord::Base
     Appsetup.sendmail(I18n.t("reportmail.toaddress"),["sohara_contact9022@jcom.home.ne.jp"],
       I18n.t("reportmail.subject").gsub("date",datestr.month.to_s+datestr.day.to_s),mailtext+I18n.t("usual.newline")+I18n.t("reportmail.footer"))
   end
+=end
+
 end
--- app/models/taskmail.rb
+++ app/models/taskmail.rb
@@ -5,15 +5,27 @@ class Taskmail < ActiveRecord::Base
     taskmail = Taskmail.new
     taskmail.from="sohara_contact9022@jcom.home.ne.jp"
     datestr=Time.now
-    taskmail.subject=I18n.t("reportmail.subject").gsub("date",datestr.month.to_s+datestr.day.to_s)
-    taskmail.header=I18n.t("reportmail.header").gsub(/name/, I18n.t("reportmail.name")) +I18n.t("usual.newline");
+    
+    if datestr.month.to_s.length == 1
+      date_month = "0" + datestr.month.to_s
+    else
+      date_month = datestr.month.to_s
+    end
+    if datestr.day.to_s.length == 1
+      date_day = "0" + datestr.day.to_s
+    else
+      date_day = datestr.month.to_s
+    end
+    
+    taskmail.subject = "Daily report_" + date_month+date_day
+    taskmail.header = "Thank you for your continued help." +  I18n.t("usual.newline") + "This is a report of the work of today.";
     taskmail.text=""
-    taskmail.footer=I18n.t("reportmail.footer")
+    taskmail.footer = "I hope the business will go well for both of us."+  I18n.t("usual.newline") +"From Seijyun Sohara."
     num=0
     for text in tasks      
       num=num+1
       taskmail.text=taskmail.text+num.to_s+I18n.t("usual.doublespace")+I18n.t("usual.colon")+I18n.t("usual.doublespace")+
-        text.name+I18n.t("usual.doublespace")+text.goal.to_s+I18n.t("usual.newline")
+        text.name+I18n.t("usual.doublespace")+I18n.t("usual.newline")+text.memo.to_s+I18n.t("usual.newline")
     end
     taskmail
   end
