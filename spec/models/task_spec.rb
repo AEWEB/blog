525d95cb97aa464e865b0751064b0d1a0829da41
webpla <sohara-others-3579@tempo.ocn.ne.jp>
2015-02-24 07:05:52 +0900

    code test.

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -6,6 +6,10 @@ RSpec.describe Task, :type => :model do
     task = Task.new(name:"ネーム",goal:"ゴール",memo:"メモ",start_time:"2015-01-03 00:00:00",end_time:"2015-01-03 00:00:00",
       security:"0",state:"1",priority:"2",category_id: "1")
     expect(task.valid?).to eq(true)
+    
+    task = Task.new(name:"ネーム",goal:"ゴール",memo:"メモ",start_time:"2015-01-03 00:00:00",end_time:"2015-01-03 00:00:00",
+      security:"0",state:"1",priority:"2",category_id: nil)
+    expect(task.valid?).to eq(true)
   end
   
   
